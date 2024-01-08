46e2f5c86902b2dab25fb55cafbc6ea4281c1700
Daumss <likedaum23@gmail.com>
2024-01-08 22:10:48 +0900

    walls

---
+++ Assets/Script/MeshCombiine.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[RequireComponent(typeof(MeshRenderer), typeof(MeshFilter))]
+public class CombineMesh : MonoBehaviour
+{
+    public GameObject[] go;
+
+    void Start()
+    {
+//        Debug.Log(transform.childCount);
+//        for(int i = 0; i<transform.childCount;i++)
+//        {
+//            Debug.Log(transform.GetChild(i).name);
+//            go[i]= transform.GetChild(i).gameObject;
+//        }
+
+        MeshFilter[] meshFilters = new MeshFilter[go.Length];
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+
+        for (int i = 0; i < meshFilters.Length; i++)
+        {
+            meshFilters[i] = go[i].GetComponent<MeshFilter>();
+            combine[i].mesh = meshFilters[i].mesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+        }
+
+        Mesh mesh = this.transform.GetComponent<MeshFilter>().mesh;
+
+        mesh.Clear();
+        mesh.CombineMeshes(combine);
+
+        this.gameObject.AddComponent<MeshCollider>();
+
+#if UNITY_EDITOR
+        { // Mesh ¿˙¿Â
+            string path = "Assets/MyMesh.asset";
+            AssetDatabase.CreateAsset(transform.GetComponent<MeshFilter>().mesh, AssetDatabase.GenerateUniqueAssetPath(path));
+            AssetDatabase.SaveAssets();
+        }
+#endif
+    }
+
+}
